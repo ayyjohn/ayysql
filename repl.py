8ffbd9aa7004c175b1b75211b0d27a2d61ff6ffc
Alec Johnson <alecjohnson55@gmail.com>
2020-07-12 20:46:02 -0700

    strip user input of trailing and leading whitespace

--- repl.py
+++ repl.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     while True:
         print("ayysql> ", end="")
-        user_input = input()
+        user_input = input().strip()
         if user_input == ".exit":
             sys.exit(0)
         else:
