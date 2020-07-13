98a55b1f3bea017a295ceb8f45c9cf827afa558f
Alec Johnson <alecjohnson55@gmail.com>
2020-07-12 20:43:00 -0700

    add a repl that lets you exit

---
+++ repl.py
@@ -0,0 +1,11 @@
+import sys
+
+if __name__ == "__main__":
+    while True:
+        print("ayysql> ", end="")
+        user_input = input()
+        if user_input == ".exit":
+            sys.exit(0)
+        else:
+            print(f"unrecognized command {user_input}")
+
