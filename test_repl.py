3fa62bd1449a646c7835b282925ca57f19170e8a
Alec Johnson <alecjohnson55@gmail.com>
2020-07-14 21:27:22 -0700

    add test for too many rows

--- test_repl.py
+++ test_repl.py
@@ -13,10 +13,25 @@ def run_script(commands):
 
 
 def test__insert__when_called__creates_an_entry():
-    result = run_script(["insert 1 alec alec@ayyjohn.com", "select", ".exit",])
+    script = [
+        "insert 1 alec alec@ayyjohn.com",
+        "select",
+        ".exit",
+    ]
+    result = run_script(script)
+
     assert result == [
         "ayysql> executed",
         "ayysql> (id: 1, username: alec, email: alec@ayyjohn.com)",
         "executed",
         "ayysql> ",
     ]
+
+
+def test__insert__too_many_rows__returns_error():
+    script = [f"insert {i} alec#{i} alec{i}@ayyjohn.com" for i in range(101)]
+    script.append(".exit")
+
+    result = run_script(script)
+
+    assert result[-2] == "ayysql> error: table full"
