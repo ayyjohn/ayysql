61b15a46272129c47cd5309f529c7105c622f739
Alec Johnson <alecjohnson55@gmail.com>
2020-07-17 08:17:33 -0700

    replace strings with constants

--- repl.py
+++ repl.py
@@ -11,12 +11,15 @@ from commands import (
     PrepareStatementResult,
 )
 
+from constants import NAME, PROMPT, EXECUTED, ERROR
+from commands import INSERT, SELECT, META_EXIT
+
 
 def run_repl():
     # type: () -> None
     table = Table()
     while True:
-        print("ayysql> ", end="")
+        print(PROMPT, end=" ")
 
         user_input = input().strip()
 
@@ -25,23 +28,23 @@ def run_repl():
             if meta_command_result == MetaCommandResult.SUCCESS:
                 continue
             elif meta_command_result == MetaCommandResult.UNRECOGNIZED_COMMAND:
-                print(f"unrecognized command {user_input}")
+                print(f"unrecognized meta command {user_input}")
                 continue
         else:
             prepare_statement_result, statement = prepare_statement(user_input)
             if prepare_statement_result == PrepareStatementResult.UNRECOGNIZED_STATEMENT:
-                print(f"Unrecognized keyword at start of '{user_input}'")
+                print(f"unrecognized keyword at start of '{user_input}'")
                 continue
             elif prepare_statement_result == PrepareStatementResult.FIELD_TOO_LONG:
-                print("error: a field is too long")
+                print(f"{ERROR} a field is too long")
                 continue
 
             execute_result = execute_statement(statement, table)
 
             if execute_result == ExecuteStatementResult.SUCCESS:
-                print("executed")
+                print(EXECUTED)
             elif execute_result == ExecuteStatementResult.TABLE_FULL:
-                print("error: table full")
+                print(f"{ERROR} table full")
 
 
 if __name__ == "__main__":
