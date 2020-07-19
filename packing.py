5c2bb29aa15afd97b6eac4b93160757310d74085
Alec Johnson <alecjohnson55@gmail.com>
2020-07-18 21:12:50 -0700

    add helper methods for packing and unpacking data

---
+++ packing.py
@@ -0,0 +1,26 @@
+from struct import calcsize, pack_into, unpack_from
+from typing import Tuple, Union
+
+from constants import NULL, UTF8
+
+# to pack/unpack a row, we need to know the types
+# currently a row consists of an id (int), and two strings of length 32 and 255
+ROW_FORMAT = "i32s255s"
+ROW_SIZE = calcsize(ROW_FORMAT)
+
+
+def serialize_row(row, page, offset):
+    # type: (Row, bytearray, int) -> None
+    return pack_into(ROW_FORMAT, page, offset, row.id, b(row.username), b(row.email))
+
+
+def b(s):
+    return bytes(s, UTF8)
+
+
+def deserialize_row(page, offset):
+    # type: (bytearray, int) -> Tuple[int, Text, Text]
+    id, name, email = unpack_from(ROW_FORMAT, page, offset)
+    name = name.decode(UTF8).rstrip(NULL)
+    email = email.decode(UTF8).rstrip(NULL)
+    return (id, name, email)
